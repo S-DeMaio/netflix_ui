01c8832b4d28ef4adfa72fa32a1183a36497d927
demaio89 <demaio89@gmail.com>
2021-11-21 15:08:48 -0600

    Setting up API requests with axios, creating Movies components and general layout of app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import Movies from './components/Movies';
+
+const URL = "https://api.themoviedb.org/3";
+const API_KEY = "4856fbe893d0ae41582688610ce19cd8";
+
+const endpoints = {
+  originals: "/discover/tv",
+  trending: "/trending/all/week",
+  now_playing: "/movie/now_playing",
+  popular: "/movie/popular",
+  top_rated: "/movie/top_rated",
+  upcoming: "/movie/upcoming",
+};
 
 function App() {
+  const [originals, setOriginals] = useState([]);
+  const [trending, setTrending] = useState([]);
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const [popular, setPopular] = useState([]);
+  const [topRated, setTopRated] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+
+  useEffect(() => {
+    // Load Originals
+    axios.get(`${URL}${endpoints.originals}`, {
+      params: {
+        api_key: API_KEY,
+      },
+    }).then((res) => setOriginals(res.data.results));
+
+    // Load Trending
+    axios.get(`${URL}${endpoints.trending}`, {
+      params: {
+        api_key: API_KEY,
+      },
+    }).then((res) => setTrending(res.data.results));
+
+    //Load now playing
+    axios.get(`${URL}${endpoints.now_playing}`, {
+      params: {
+        api_key: API_KEY,
+      },
+    }).then((res) => setNowPlaying(res.data.results));
+
+    //Load popular 
+    axios.get(`${URL}${endpoints.popular}`, {
+      params: {
+        api_key: API_KEY,
+      },
+    }).then((res) => setPopular(res.data.results));
+
+    // load top rated
+    axios
+      .get(`${URL}${endpoints.top_rated}`, {
+        params: {
+          api_key: API_KEY,
+        },
+      })
+      .then((res) => setTopRated(res.data.results));
+
+    // load upcoming 
+    axios.get(`${URL}${endpoints.upcoming}`, {
+      params: {
+        api_key: API_KEY,
+      },
+    }).then((res) => setUpcoming(res.data.results));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+      <Movies title="Quckflix Originals" movies={originals} />
+      <Movies title="Trending" movies={trending} />
+      <Movies title="Now Playing" movies={nowPlaying} />
+      <Movies title="Popular" movies={popular} />
+      <Movies title="Top Rated" movies={topRated} />
+      <Movies title="Upcoming" movies={upcoming} />
+    </>
   );
 }
 
